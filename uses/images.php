29ef1182979ecf34e34c535b35e5a9e1e4227357
Nazar Kuliyev <nazar.kuliev@gmail.com>
2011-07-12 14:26:32 +0600

    -

--- uses/images.php
+++ uses/images.php
@@ -22,12 +22,24 @@ function page_admin_images($act="",$id="") {
 
   if(form_file_uploaded("file")) {
 	$fname = $_FILES["file"]['name'];
-	db_query("INSERT INTO images (link) VALUES ('')");
-	$id = db_last_id();
-	$fname = $id.".".fileext($fname);
-	form_file_uploaded_move("file","img/".$fname);
-	db_query("UPDATE images SET link='img/$fname' WHERE id=%d",$id);
-	redir("admin/images");
+    $ext  = strtolower(fileext($fname));
+	if(!($ext=="swf" || $ext=="jpg" || $ext=="gif" || $ext=="png" || $ext=="bmp" || $ext=="jpeg" || $ext=="pdf")) {
+	  $o = "Данный тип файла не является картинкой";       
+	  return $o;
+	} else {
+		if($act=="add") {
+		  db_query("INSERT INTO images (link) VALUES ('')");
+		  $id = db_last_id();
+		} else {
+		  @unlink(db_result(db_query("SELECT link FROM images WHERE id=%d",$id)));
+		}
+
+		$fname = $id.".".fileext($fname);
+
+		form_file_uploaded_move("file","img/".$fname);
+		db_query("UPDATE images SET link='img/$fname' WHERE id=%d",$id);
+		redir("admin/images");
+	}
   }
 
   if($act=="add") {
@@ -38,7 +50,7 @@ function page_admin_images($act="",$id="") {
 
   if($act=="del") {
     $im = db_object_get("images",$id);
-	@unlink("../$im->link");
+	unlink("$im->link");
   }
 
   $o = table_edit("images","admin/images",$act,$id,"","","","image_func");
