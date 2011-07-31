6ddf2ba29b3dc6cdec803a5cb1867e1d0533c5dc
Nazar Kuliyev <nazar.kuliev@gmail.com>
2011-07-31 17:10:56 +0600

    form_email

--- uses/form_email.php
+++ uses/form_email.php
@@ -1,5 +1,7 @@
 <?php
 
+//to put inline
+$template_call['frm'] = true;
 function frm($name) {
   if(form_post("submit")) {
 	    $message = "";
@@ -11,11 +13,11 @@ function frm($name) {
 
           if(str_beg($key,"f_")) {
 			 $caption = $_REQUEST["c_".str_replace("f_","",$key)];
-             $message .= "$caption\r\n$value\r\n\r\n";
+             $message .= "$caption:\r\n$value\r\n\r\n";
 		  }
 		}
 
-		$from = "no-reply@tradecity.kz";
+		$from = "no-reply-site-form@".$_SERVER['HTTP_HOST'];
 		$to = setting("admin_email");
 		$subject = form_post("subject");
 		$local = ($_SERVER['REMOTE_ADDR'] == "127.0.0.1");
@@ -23,15 +25,15 @@ function frm($name) {
 		if(form_file_uploaded("uploadedfile")) {
 		   $tmp = $_FILES['uploadedfile']['tmp_name'];
 		   $fname = $_FILES['uploadedfile']['name'];
-           if($local) {
+/*           if($local) {
              die("<pre>$message tmp[$tmp] fname[$fname]</pre>");
-		   } else {
+		   } else {*/
              mail_attach($from,$to,$subject,$message,$tmp,$fname);
-		   }
+//		   }
 		} else {
-           if($local) {
+/*           if($local) {
              die("<pre>$message</pre>");
-		   } else
+		   } else*/
 		     mail_from($to,$subject,$message,$from);
 		}
 
@@ -39,7 +41,8 @@ function frm($name) {
 		return form_post("success");
 
   }
-  return template("frm_".$name);
+
+  return template("form_email","content",template("form_email_".$name));
 }
 
 function page_form($name) {
